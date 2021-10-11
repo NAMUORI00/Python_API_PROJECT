50d14f48f4a287c4a747bbf3005048ef2ea0294f
Ebisuzawa Kei <greentree329@naver.com>
2021-10-11 18:27:01 +0900

    xml의 유효정보를 딕셔너리로 불러왔습니다

--- xml_func.py
+++ xml_func.py
@@ -7,9 +7,11 @@ vaildroadid = 4000000000 #DBF에서 제주도 도로가 시작되는 지점
 def xmllistdump(file_path):
     xml_tree = API_REQUEST.api_request()  # API 리퀘스트 후, XML 반환 데이터를 xml_tree 에 담기
     temp_list = list()
-    for child in xml_tree.iter('LINK_ID'):
-        if int(child.text) >= vaildroadid: # DBF 파일 내의 제주도 영역이 시작되는 지점의 LINKID 부터 XML에서 로드
-            temp_list.append(child)
+    for child in xml_tree.iter('list'):
+        alpha = list(child)
+        if int(alpha[0].text) >= vaildroadid: # DBF 파일 내의 제주도 영역이 시작되는 지점의 LINKID 부터 XML에서 로드
+            node = {'LINKID':alpha[0].text, 'OCPY_RATE':alpha[1].text, 'PRCN_DT':alpha[2].text, 'SPED':alpha[3].text, 'TFVL':alpha[4].text, 'TRVL_HH':alpha[5].text}
+            temp_list.append(node)
     with open(file_path, 'wb') as file:
         pickle.dump(temp_list, file)
     file.close()
@@ -25,8 +27,8 @@ def xmldumpopen(file_path):
 
 #xml_list의 LINK_ID중 최소값과 최대값을 찾습니다
 def minmaxlinkid(xml_list):
-    minmax = [int(xml_list[0].text),0]
+    minmax = [int(xml_list[0]['LINKID']),0]
     for count in range(len(xml_list)):
-        minmax[0] = int(xml_list[count].text) if minmax[0] > int(xml_list[count].text) else minmax[0]
-        minmax[1] = int(xml_list[count].text) if minmax[1] < int(xml_list[count].text) else minmax[1]
+        minmax[0] = int(xml_list[count]['LINKID']) if minmax[0] > int(xml_list[count]['LINKID']) else minmax[0]
+        minmax[1] = int(xml_list[count]['LINKID']) if minmax[1] < int(xml_list[count]['LINKID']) else minmax[1]
     return minmax
