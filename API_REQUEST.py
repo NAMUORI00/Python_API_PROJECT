5c588931e707c1183c9d744fbe987a0177c149ea
TheBlueDream <toolbar@jejunu.ac.kr>
2021-12-01 20:21:37 +0900

    API 리퀘스트 폐지로 인한 XML 로드로 변경

--- API_REQUEST.py
+++ API_REQUEST.py
@@ -10,9 +10,15 @@ from xml.etree import ElementTree
 # xml_tree = API_REQUEST.api_request()
 
 # xml 리턴
+
+# xml 리턴
 def api_request():
-    apikey = unquote("AU%2B3Jd9M%2F4EFoqDe43gUAyFC9QwBPwKS0grAx3kbZCqeAlO9yeNzqYh0OZT0gKYtAwVeJVkihmP8MyopkaQPfg%3D%3D")
-    paramdata = {"authApiKey": apikey}
-    resp = requests.get('http://openapi.jejuits.go.kr/rfcapi/rest/jejuits/getTrafficInfo', params=paramdata)
-    tree = ElementTree.fromstring(resp.content)
+    tree = ElementTree.parse('dump_file/getTrafficInfo.xml')
     return tree
+
+#def api_request():
+#    apikey = unquote("AU%2B3Jd9M%2F4EFoqDe43gUAyFC9QwBPwKS0grAx3kbZCqeAlO9yeNzqYh0OZT0gKYtAwVeJVkihmP8MyopkaQPfg%3D%3D")
+#    paramdata = {"authApiKey": apikey}
+#    resp = requests.get('http://openapi.jejuits.go.kr/rfcapi/rest/jejuits/getTrafficInfo', params=paramdata)
+#    tree = ElementTree.fromstring(resp.content)
+#    return tree
